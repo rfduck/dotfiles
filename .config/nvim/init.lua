be654f15aa4f1ccb2e399309d8b4b300c11b1047
Russell Fegles <rfegles1@gmail.com>
2023-08-12 13:29:07 -0700

    Adding init.lua to dotfiles

---
+++ .config/nvim/init.lua
@@ -0,0 +1,25 @@
+require('settings')
+require('keymaps')
+
+-------
+-------Lazy.nvim plugin manager
+-------
+-- Setup:
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+--require
+require("lazy").setup("plugins")
+
+-- Color Scheme
+vim.cmd([[colorscheme bluedrake]])
