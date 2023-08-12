c5dbe1a211d511f3272703066643a51ee98c5b7d
Russell Fegles <rfegles1@gmail.com>
2023-08-12 13:32:56 -0700

    Adding zen-mode config to dotfiles

---
+++ .config/nvim/lua/plugins/zen-mode/init.lua
@@ -0,0 +1,16 @@
+return {
+        "folke/zen-mode.nvim",
+        opts = {
+            window = {
+                backdrop = 0.9,
+                width = 0.5,
+                height = 0.7,
+                options = {
+                    number = false,
+                },
+            },
+        plugins = {
+            twilight = { enabled = true }
+        },
+    }
+}
