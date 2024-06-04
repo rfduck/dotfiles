ef309e1c2d1650dd03b520f40d3bd12d676bd107
Russell Fegles <rfegles1@gmail.com>
2024-06-03 23:37:06 -0700

    Added config section with code that lets zoxide use fzf-lua

--- .config/nvim/lua/plugins/zoxide.lua
+++ .config/nvim/lua/plugins/zoxide.lua
@@ -1,5 +1,16 @@
 --zoxide plugin
 
 return {
-'nanotee/zoxide.vim'
+'nanotee/zoxide.vim',
+
+cmd = { 'Z', 'Zi' },
+
+config = 
+--This lets zoxide use fzf-lua.nvim
+    function()
+        require('fzf-lua').register_ui_select()
+        vim.g.zoxide_use_select = 1
+    end,
+
 }
+
