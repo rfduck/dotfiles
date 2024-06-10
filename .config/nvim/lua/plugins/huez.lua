3fcdc12fddfea89c08ba5442fee0eaa51fc3735d
Russell Fegles <rfegles1@gmail.com>
2024-06-09 23:00:55 -0700

    Re-added huez because there's a new version that works better

---
+++ .config/nvim/lua/plugins/huez.lua
@@ -0,0 +1,12 @@
+--huez colorscheme previews
+
+return {
+    "vague2k/huez.nvim",
+    -- if you want registry related features, uncomment this
+    import = "huez-manager.import",
+    branch = "stable",
+    event = "UIEnter",
+    config = function()
+        require("huez").setup({})
+    end,
+}
